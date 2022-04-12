333cdc7e21e513fee8a5a0d1a1601951cf97b3d5
tylo-zane <43626511+tylo-zane@users.noreply.github.com>
2022-04-11 17:04:27 -0700

    added Javascript skeleton

---
+++ index.js
@@ -0,0 +1,14 @@
+(function() {
+    "use strict";
+ 
+    window.addEventListener("load", initialize);
+ 
+   /**
+    * Initializes the webpage by adding event listeners to each button.
+    */
+    function initialize() {
+
+    }
+ 
+ })();
+ 
