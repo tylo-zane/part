a9ce4464a5fadf6a53c3e5128d22d623d845127c
tylo-zane <43626511+tylo-zane@users.noreply.github.com>
2022-04-11 17:26:38 -0700

    added webcam

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 (function() {
     "use strict";
+
+    const modelParams = {
+        flipHorizontal: false,   // flip e.g for video 
+        imageScaleFactor: 0.7,  // reduce input image size .
+        maxNumBoxes: 2,        // maximum number of boxes to detect
+        iouThreshold: 0.5,      // ioU threshold for non-max suppression
+        scoreThreshold: 0.79,    // confidence threshold for predictions.
+      }
+      
+    const img = document.getElementById('webcam');
  
     window.addEventListener("load", initialize);
  
@@ -7,7 +17,13 @@
     * Initializes the webpage by adding event listeners to each button.
     */
     function initialize() {
-
+        handTrack.startVideo(img);
+        console.log("initialized");
+        handTrack.load(modelParams).then(model => {
+            model.detect(img).then(predictions => {
+                console.log('Predictions: ', predictions); 
+            });
+        });
     }
  
  })();
