87ba9ab23c2630e7333d08f3964c812cb28b3737
tylo-zane <43626511+tylo-zane@users.noreply.github.com>
2022-05-31 20:28:48 -0700

    added fullscreen mode

--- index.js
+++ index.js
@@ -248,6 +248,23 @@
         let h2 = document.querySelector("h2");
         h2.classList.remove("score");
     }
+
+    function openFullscreen() {
+        let docElm = document.documentElement;
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        }
+        else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+        }
+        else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+        }
+        else if (docElm.msRequestFullscreen) {
+            docElm.msRequestFullscreen();
+        }
+
+    }
     
 /*     // Load the model.
     handTrack.load(modelParams).then(lmodel => {
@@ -263,6 +280,7 @@
     */
     function initialize() {
         console.log("initialized");
+        document.getElementById("full-button").addEventListener("click", openFullscreen);
         startVideo();
     }
  
